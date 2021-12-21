6603380422842dde5fb11928d78909989bae589c
coming1024 <57028337+coming1024@users.noreply.github.com>
2021-12-21 18:00:50 +0800

    Add files via upload

---
+++ BackEnd/position-service/src/main/java/org/example/Controller.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags="时序数据库接口")
+@RestController
+@RequestMapping(value="/influxDB")
+public class Controller {
+
+    @ApiOperation("查询数据库")
+    @ApiImplicitParam(name = "params", value = "查询参数",dataType = "String")
+    @GetMapping(value="/getData")
+    public String test(@RequestParam String params){
+        return "服务1时序数据库"+params;
+    }
+
+}
