c58a61be8967bddc21de8b8ed77fc629bf6a2930
yifan <1042750083@qq.com>
2021-12-23 02:33:01 +0800

    .

--- FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/OrderFragment.java
+++ FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/OrderFragment.java
@@ -13,7 +13,10 @@ public class OrderFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_order,container,false);
     }
 }
