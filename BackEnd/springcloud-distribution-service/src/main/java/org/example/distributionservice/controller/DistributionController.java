89322a53db82220fe2895174dc333bc2bd902740
LuBixing <1172940427@qq.com>
2021-12-23 02:34:08 +0800

    尝试调整swagger无果

--- BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/controller/DistributionController.java
+++ BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/controller/DistributionController.java
@@ -3,12 +3,11 @@ package org.example.distributionservice.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.example.distributionservice.service.DistributionService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Api(tags = "派单服务接口")
+@Api(tags = "派单")
+@CrossOrigin("*")
 public class DistributionController {
     @RequestMapping(value="/distribution",method = RequestMethod.GET)
     @ApiOperation("获取派单信息")
