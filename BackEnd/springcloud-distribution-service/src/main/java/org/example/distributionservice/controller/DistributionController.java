aa8c3f7b0bcd54254d86f7f1826e37f718607221
LuBixing <1172940427@qq.com>
2021-12-23 02:05:47 +0800

    Update DistributionController.java

--- BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/controller/DistributionController.java
+++ BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/controller/DistributionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DistributionController {
     @RequestMapping(value="/distribution",method = RequestMethod.GET)
     @ApiOperation("获取派单信息")
-    public void test(){
+    public int[] test(){
         DistributionService distributionService = new DistributionService(2,2);
         distributionService.distribute();
         return distributionService.getResult();
