7dbd5d751ed1acc2b209d60e01deb162488ca773
Sry-maker <1961384758@qq.com>
2021-12-23 15:19:59 +0800

    前端初步

--- FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/ForgotpwdActivity.java
+++ FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/ForgotpwdActivity.java
@@ -1,159 +1,48 @@
 package com.example.onlinecarhailing;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import android.app.Activity;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.util.Map;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class SignupTabFragment extends Fragment {
+public class ForgotpwdActivity extends AppCompatActivity {
 
-    //声明变量
-    EditText phone_number, password, identity, verification_code, username;
-    Button signup,getcode0;
 
-    //用来设置动画透明度的
-    float v = 0;
+    EditText phone_number, newpassword, verification_code, username;
+    Button resignup,getcode;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_signup_tab, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotpwd);
 
-        //把变量和控件进行绑定
-        phone_number = root.findViewById(R.id.phone_number);
-        password = root.findViewById(R.id.password);
-//        confirm = root.findViewById(R.id.confirm);
-        signup = root.findViewById(R.id.signup);
-        getcode0=root.findViewById(R.id.getcode);
-        identity = root.findViewById(R.id.identity);
-        verification_code = root.findViewById(R.id.verification_code);
-        username = root.findViewById(R.id.username);
+        phone_number=findViewById(R.id.phone_number1);
+        newpassword=findViewById(R.id.newpwd1);
+        verification_code=findViewById(R.id.verification_code1);
+        username=findViewById(R.id.myname1);
 
-        //切换为"登录"时的动画设置
-        phone_number.setTranslationY(800);
-        password.setTranslationY(800);
-//        confirm.setTranslationY(800);
-        phone_number.setAlpha(v);
-        password.setAlpha(v);
-//        confirm.setAlpha(v);
-        phone_number.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-        password.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-//        confirm.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        getcode=findViewById(R.id.getcode1);
+        resignup=findViewById(R.id.login1);
 
-        //注册按钮的响应
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //捕获按键对应的文本
-
-                String _phone_number = phone_number.getText().toString();
-                String _password = password.getText().toString();
-                String _identity = identity.getText().toString();
-                String _verification_code = verification_code.getText().toString();
-                String _username = username.getText().toString();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            FormBody.Builder params = new FormBody.Builder();
-                            params.add("name", _username);
-                            params.add("passwd", _password);
-                            params.add("phone", _phone_number);
-                            params.add("identifyCode", _verification_code);
-                            params.add("identity", _identity);
-
-                            OkHttpClient client = new OkHttpClient();
-                            Request request = new Request.Builder()
-                                    .url("http://47.103.9.250:9000/api/v1/userservice/registration")
-//                                    .url("http://127.0.0.1:9000/api/v1/userservice/registration")
-                                    .post(params.build())
-                                    .build();
-                            Response response = client.newCall(request).execute();
-                            String responseData = response.body().string();
-                            JSONObject jsonObject = new JSONObject(responseData);
-                            Log.d("msg", jsonObject.getString("msg"));
-                            Log.d("object", jsonObject.getString("object"));
-                            String object = jsonObject.getString("object");
-                            String code=jsonObject.getString("code");
-
-                            if(code.equals("200")){
-                                getActivity().runOnUiThread( new  Runnable() {
-                                    @Override
-                                    public  void  run() {
-                                        Toast toastCenter = Toast.makeText(getActivity(), "注册成功\n已自动登录", Toast.LENGTH_LONG);
-                                        //确定Toast显示位置，并显示
-                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
-                                        toastCenter.show();
-                                    }
-                                });
-
-
-                            }
-                            else {
-                                getActivity().runOnUiThread( new  Runnable() {
-                                    @Override
-                                    public  void  run() {
-                                        Toast toastCenter = Toast.makeText(getActivity(), "注册失败", Toast.LENGTH_LONG);
-                                        //确定Toast显示位置，并显示
-                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
-                                        toastCenter.show();
-                                    }
-                                });
-                            }
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            getActivity().runOnUiThread( new  Runnable() {
-                                @Override
-                                public  void  run() {
-                                    Toast toastCenter = Toast.makeText(getActivity(), "注册失败", Toast.LENGTH_LONG);
-                                    //确定Toast显示位置，并显示
-                                    toastCenter.setGravity(Gravity.CENTER, 0, 0);
-                                    toastCenter.show();
-                                }
-                            });
-                        }
-
-                    }
-                }).start();
-
-            }
-        });
-        getcode0.setOnClickListener(new View.OnClickListener() {
+        getcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String _phone_number = phone_number.getText().toString();
-                String _password = password.getText().toString();
-                String _identity = identity.getText().toString();
+                String _password = newpassword.getText().toString();
                 String _verification_code = verification_code.getText().toString();
                 String _username = username.getText().toString();
 
@@ -164,13 +53,13 @@ public class SignupTabFragment extends Fragment {
                     public void run() {
                         try {
                             FormBody.Builder params = new FormBody.Builder();
-
+                            params.add("name",_username);
                             params.add("phone", _phone_number);
 
 
                             OkHttpClient client = new OkHttpClient();
                             Request request = new Request.Builder()
-                                    .url("http://47.103.9.250:9000/api/v1/userservice/registercode")
+                                    .url("http://47.103.9.250:9000/api/v1/userservice/resetphonecode")
 //                                    .url("http://127.0.0.1:9000/api/v1/userservice/registration")
                                     .post(params.build())
                                     .build();
@@ -183,10 +72,10 @@ public class SignupTabFragment extends Fragment {
                             String code=jsonObject.getString("code");
 
                             if(code.equals("200")){
-                                getActivity().runOnUiThread( new  Runnable() {
+                                runOnUiThread( new  Runnable() {
                                     @Override
                                     public  void  run() {
-                                        Toast toastCenter = Toast.makeText(getActivity(), "获取验证码成功", Toast.LENGTH_LONG);
+                                        Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "获取验证码成功", Toast.LENGTH_LONG);
                                         //确定Toast显示位置，并显示
                                         toastCenter.setGravity(Gravity.CENTER, 0, 0);
                                         toastCenter.show();
@@ -195,10 +84,10 @@ public class SignupTabFragment extends Fragment {
 
                             }
                             else {
-                                getActivity().runOnUiThread( new  Runnable() {
+                                runOnUiThread( new  Runnable() {
                                     @Override
                                     public  void  run() {
-                                        Toast toastCenter = Toast.makeText(getActivity(), "获取验证码失败", Toast.LENGTH_LONG);
+                                        Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "获取验证码失败", Toast.LENGTH_LONG);
                                         //确定Toast显示位置，并显示
                                         toastCenter.setGravity(Gravity.CENTER, 0, 0);
                                         toastCenter.show();
@@ -208,10 +97,10 @@ public class SignupTabFragment extends Fragment {
 
                         } catch (Exception e) {
                             e.printStackTrace();
-                            getActivity().runOnUiThread( new  Runnable() {
+                            runOnUiThread( new  Runnable() {
                                 @Override
                                 public  void  run() {
-                                    Toast toastCenter = Toast.makeText(getActivity(), "获取验证码失败", Toast.LENGTH_LONG);
+                                    Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "获取验证码失败", Toast.LENGTH_LONG);
                                     //确定Toast显示位置，并显示
                                     toastCenter.setGravity(Gravity.CENTER, 0, 0);
                                     toastCenter.show();
@@ -225,7 +114,85 @@ public class SignupTabFragment extends Fragment {
             }
         });
 
-        return root;
+        resignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String _phone_number = phone_number.getText().toString();
+                String _password = newpassword.getText().toString();
+                String _verification_code = verification_code.getText().toString();
+                String _username = username.getText().toString();
+
+
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            FormBody.Builder params = new FormBody.Builder();
+                            params.add("name",_username);
+                            params.add("phone", _phone_number);
+                            params.add("newpasswd",_password);
+                            params.add("identifyCode",_verification_code);
+
+                            OkHttpClient client = new OkHttpClient();
+                            Request request = new Request.Builder()
+                                    .url("http://47.103.9.250:9000/api/v1/userservice/resetpasswords")
+//                                    .url("http://127.0.0.1:9000/api/v1/userservice/registration")
+                                    .post(params.build())
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            String responseData = response.body().string();
+                            JSONObject jsonObject = new JSONObject(responseData);
+                            Log.d("msg", jsonObject.getString("msg"));
+                            Log.d("object", jsonObject.getString("object"));
+                            String object = jsonObject.getString("object");
+                            String code=jsonObject.getString("code");
+
+                            if(code.equals("200")){
+                                runOnUiThread( new  Runnable() {
+                                    @Override
+                                    public  void  run() {
+                                        Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "重置密码成功", Toast.LENGTH_LONG);
+                                        //确定Toast显示位置，并显示
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+                                    }
+                                });
+                                Intent intent0 = new Intent(ForgotpwdActivity.this, LoginTabFragment.class);
+                                startActivity(intent0);
+
+                            }
+                            else {
+                                runOnUiThread( new  Runnable() {
+                                    @Override
+                                    public  void  run() {
+                                        Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "重置密码失败", Toast.LENGTH_LONG);
+                                        //确定Toast显示位置，并显示
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+                                    }
+                                });
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            runOnUiThread( new  Runnable() {
+                                @Override
+                                public  void  run() {
+                                    Toast toastCenter = Toast.makeText(ForgotpwdActivity.this, "重置密码失败", Toast.LENGTH_LONG);
+                                    //确定Toast显示位置，并显示
+                                    toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                    toastCenter.show();
+                                }
+                            });
+                        }
+
+                    }
+                }).start();
+
+            }
+        });
 
     }
 }
--- FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/ProfileFragment.java
+++ FrontEnd/OnlineCarHailing/app/src/main/java/com/example/onlinecarhailing/ProfileFragment.java
@@ -1,19 +1,228 @@
 package com.example.onlinecarhailing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class ProfileFragment extends Fragment {
+
+    EditText phone_number, commonaddress1, commonaddress2, username, truename;
+    Button change2;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Intent intenttest = getActivity().getIntent();
+        String _username = intenttest.getStringExtra("name");
+        change2 = root.findViewById(R.id.change2);
+        username = root.findViewById(R.id.username2);
+        phone_number = root.findViewById(R.id.phone_number2);
+        truename = root.findViewById(R.id.truename2);
+        commonaddress1 = root.findViewById(R.id.commonaddress1);
+        commonaddress2 = root.findViewById(R.id.commonaddress2);
+        username.setText(_username);
+        final String[] _phone_number = {null};
+        final String[] _truename = {null};
+        final String[] _commonaddress1 = {null};
+        final String[] _commonaddress2 = {null};
+        final String[] passwd = {null};
+        final String[] salt = {null};
+
+//        String _username = username.getText().toString();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    FormBody.Builder params = new FormBody.Builder();
+                    params.add("name", _username);
+
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://47.103.9.250:9000/api/v1/userservice/returnclientchage")
+//                                    .url("http://127.0.0.1:9000/api/v1/userservice/registration")
+                            .post(params.build())
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    JSONObject jsonObject = new JSONObject(responseData);
+//                    Log.d("msg", jsonObject.getString("msg"));
+//                    Log.d("object", jsonObject.getString("object"));
+//                    String object = jsonObject.getString("object");
+                    String code = jsonObject.getString("code");
+                    JSONObject object2 = jsonObject.getJSONObject("object");
+                    _phone_number[0] = object2.getString("phone");
+                    _truename[0] = object2.getString("truename");
+                    _commonaddress1[0] = object2.getString("commonaddress1");
+                    _commonaddress2[0] = object2.getString("commonaddress2");
+                    passwd[0] = object2.getString("passwd");
+                    salt[0] = object2.getString("salt");
+
+                    if (code.equals("200")) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                phone_number.setText(_phone_number[0]);
+                                truename.setText(_truename[0]);
+                                commonaddress1.setText(_commonaddress1[0]);
+                                commonaddress2.setText(_commonaddress2[0]);
+                                Toast toastCenter = Toast.makeText(getActivity(), "获取个人信息成功", Toast.LENGTH_LONG);
+                                //确定Toast显示位置，并显示
+                                toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                toastCenter.show();
+                            }
+                        });
+
+//                        Intent intent = new Intent(getActivity(), MainActivity.class);
+//                        startActivity(intent);
+                    } else {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast toastCenter = Toast.makeText(getActivity(), "获取个人信息失败", Toast.LENGTH_LONG);
+                                //确定Toast显示位置，并显示
+                                toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                toastCenter.show();
+                            }
+                        });
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast toastCenter = Toast.makeText(getActivity(), "获取个人信息失败", Toast.LENGTH_LONG);
+                            //确定Toast显示位置，并显示
+                            toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                            toastCenter.show();
+                        }
+                    });
+                }
+
+            }
+        }).start();
+
+
+        change2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //捕获按键对应的文本
+                _phone_number[0] = phone_number.getText().toString();
+                _truename[0] = truename.getText().toString();
+                 _commonaddress1[0] = commonaddress1.getText().toString();
+                _commonaddress2[0] = commonaddress2.getText().toString();
+//                String _phone_number = phone_number.getText().toString();
+//                String _password = password.getText().toString();
+//                String _identity = identity.getText().toString();
+//                String _verification_code = verification_code.getText().toString();
+//                String _username = username.getText().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+//                            String json = "{\n" +
+//                                    "    \"commonaddress1\": \"string\",\n" +
+//                                    "  \"commonaddress2\": \"string\",\n" +
+//                                    "  \"id\": 0,\n" +
+//                                    "  \"name\": \"string\",\n" +
+//                                    "  \"passwd\": \"string\",\n" +
+//                                    "  \"phone\": \"string\",\n" +
+//                                    "  \"salt\": \"string\",\n" +
+//                                    "  \"truename\": \"string\"\n" +
+//                                    "}";
+                            String json = "{\r\n" +
+                                    "    \"commonaddress1\": \"" + _commonaddress1[0] + "\",\n" +
+                                    "  \"commonaddress2\": \"" + _commonaddress2[0] + "\",\n" +
+                                    "  \"name\": \"" + _username + "\",\n" +
+                                    "  \"passwd\": \"" + passwd[0] + "\",\n" +
+                                    "  \"phone\": \"" + _phone_number[0] + "\",\n" +
+                                    "  \"salt\": \"" + salt[0] + "\",\n" +
+                                    "  \"truename\": \"" + _truename[0] + "\"\n" +
+                                    "}";
+                            OkHttpClient client = new OkHttpClient().newBuilder()
+                                    .build();
+                            MediaType mediaType = MediaType.parse("application/json");
+                            RequestBody body = RequestBody.create(mediaType, json);
+                            Request request = new Request.Builder()
+                                    .url("http://47.103.9.250:9000/api/v1/userservice/clientchage")
+                                    .method("POST", body)
+                                    .addHeader("Content-Type", "application/json")
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            String responseData = response.body().string();
+                            JSONObject jsonObject = new JSONObject(responseData);
+                            String code = jsonObject.getString("code");
+
+                            if (code.equals("200")) {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast toastCenter = Toast.makeText(getActivity(), "注册成功\n已自动登录", Toast.LENGTH_LONG);
+                                        //确定Toast显示位置，并显示
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+                                    }
+                                });
+
+
+                            } else {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast toastCenter = Toast.makeText(getActivity(), "注册失败", Toast.LENGTH_LONG);
+                                        //确定Toast显示位置，并显示
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+                                    }
+                                });
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast toastCenter = Toast.makeText(getActivity(), "注册失败", Toast.LENGTH_LONG);
+                                    //确定Toast显示位置，并显示
+                                    toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                    toastCenter.show();
+                                }
+                            });
+                        }
+
+                    }
+                }).start();
+
+            }
+
+
+        });
+
+
+        return root;
     }
 }
