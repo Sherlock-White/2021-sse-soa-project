f2c3267d5be326de58f8fbce4865b122ca176ec5
coming1024 <57028337+coming1024@users.noreply.github.com>
2021-12-23 23:24:01 +0800

    Add files via upload

---
+++ BackEnd/position-service/src/main/java/com/mapper/mysql/MysqlMapper.java
@@ -0,0 +1,10 @@
+package com.mapper.mysql;
+
+/**
+ * @author CuiXi
+ * @version 1.0
+ * @Description:
+ * @date 2021/3/11 14:30
+ */
+public interface MysqlMapper {
+}
---
+++ BackEnd/position-service/src/main/java/com/test/Mate.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import lombok.Data;
+
+/**
+ * @author CuiXi
+ * @version 1.0
+ * @Description:
+ * @date 2021/3/11 11:23
+ */
+@Data
+public class Mate {
+
+    private Long ts;
+    private int id;
+}
