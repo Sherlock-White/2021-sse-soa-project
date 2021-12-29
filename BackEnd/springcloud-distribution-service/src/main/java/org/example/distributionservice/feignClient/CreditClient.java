007330014da0eaf5fae6caba8e0f09b2ebd8100b
LuBixing <1172940427@qq.com>
2021-12-29 20:09:44 +0800

    增加熔断器、删除result

--- BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/feignClient/CreditClient.java
+++ BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/feignClient/CreditClient.java
@@ -1,12 +1,13 @@
 package org.example.distributionservice.feignClient;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.xml.transform.Result;
-@FeignClient(value="userservice",url="http://47.103.9.250:9000")
+@FeignClient(value="userservice",url="http://47.103.9.250:9000",fallback = CreditClientImpl.class)
 public interface CreditClient {
-    @PostMapping("/api/v1/userservice/creditworthiness")
-    Result findDriverById(@RequestParam("name") String name);
+    @PostMapping("userservice/creditworthiness")
+    String[] findDriverById(@RequestParam("name") String[] nameList);
 }
