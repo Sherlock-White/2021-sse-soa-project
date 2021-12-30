641c56e43678a77d9cacc3a091a944082bcf3744
LuBixing <1172940427@qq.com>
2021-12-30 13:13:44 +0800

    尝试修复swagger

--- BackEnd/orderService/src/main/java/com/example/orderservice/config/SwaggerConfig.java
+++ BackEnd/orderService/src/main/java/com/example/orderservice/config/SwaggerConfig.java
@@ -1,63 +1,45 @@
 package com.example.orderservice.config;
 
-
-
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.context.request.async.DeferredResult;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.*;
 
-@Configuration  //告诉Spring容器，这个类是一个配置类
-@EnableSwagger2  //启用Swagger2
-//@EnableSwaggerBootstrapUI
-//@EnableWebMvc  //必须用这个，不然会有空指针报错问题
-public class SwaggerConfig /*implements WebMvcConfigurer*/ {
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
 
-
+    //创建接口文档
     @Bean
-    public Docket createUserApi() {
+    public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("orderService")
-                .apiInfo(apiUserInfo())
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.orderservice.controller"))// com包下所有API都交给Swagger2管理
-                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
-                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                //扫描org.example.controller包下文件
+                .apis(RequestHandlerSelectors.basePackage("org.example.controller"))
+                //扫描@Api注解的类
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //扫描@ApiOperation注解的方法
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
                 .build();
     }
 
-    /**
-     * 此处主要是API文档页面显示信息
-     */
-    private ApiInfo apiUserInfo() {
+    //接口文档的描述
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("orderAPI") // 标题
-                .description("订单") // 描述
-                .termsOfServiceUrl("https://www.tongji.edu.cn") // 服务网址，一般写公司地址
-                .version("1.0") // 版本
+                .title("订单微服务")
+                .description("订单微服务")
+                .version("1.0")
                 .build();
     }
-
-//    @Override   //重载拦截器
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
 }
-
-
