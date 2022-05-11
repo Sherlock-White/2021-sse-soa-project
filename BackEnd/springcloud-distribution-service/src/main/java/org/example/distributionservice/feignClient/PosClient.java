c00d8e1913ffce345099ca5ccd181b92709bd79d
palymaker <1953481@tongji.edu.cn>
2022-05-11 14:57:29 +0800

    syq取消订单

--- BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/feignClient/PosClient.java
+++ BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/feignClient/PosClient.java
@@ -2,14 +2,12 @@ package org.example.distributionservice.feignClient;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.xml.transform.Result;
 import java.util.List;
-import java.util.Map;
+
 //url待定
-@FeignClient(value="position-service",url="http://139.224.251.185:9004",fallback = PositionClientImpl.class)
-public interface PositionClient {
+@FeignClient(value="position-service",url="http://139.224.251.185:9004",fallback = PosClientImpl.class)
+public interface PosClient {
     @GetMapping("/position/getAll")
     List<Object> getNearDriverList();
 }
--- BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/rabbitMQ/Listener.java
+++ BackEnd/springcloud-distribution-service/src/main/java/org/example/distributionservice/rabbitMQ/Listener.java
@@ -1,37 +1,35 @@
+
 package org.example.distributionservice.rabbitMQ;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSON;
-import net.sf.json.JSONArray;
 //import net.sf.json.JSONObject;
 
-import org.apache.juli.logging.Log;
-import org.example.distributionservice.feignClient.PositionClient;
+import org.example.distributionservice.feignClient.PosClient;
 import org.example.distributionservice.service.DistributionService;
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.SecureRandom;
-import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class Listener {
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private RabbitTemplate rabbitTemplate3;//发送给用户微服务改变司机状态
     @Autowired
-    private PositionClient positionClient;
+    private RabbitTemplate rabbitTemplate2;//订单取消返回订单微服务
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Qualifier("org.example.distributionservice.feignClient.PosClient")
+    @Autowired
+    private PosClient posClient;
     //@RabbitListener(queues = {"dispatch"})
     @Transactional
 //    @RabbitListener(bindings = @QueueBinding(
@@ -60,30 +58,58 @@ public class Listener {
                 {"driver2","31.194202","121.320655"}};*/
 
         //获取周围司机
-        List<Object> driverList = positionClient.getNearDriverList();
-        System.out.println(driverList.toString());
+//        List<Object> driverList = posClient.getNearDriverList();
+//        System.out.println(driverList.toString());
         //{ts=2021-07-06 15:04:20.0, id=247, jing=30.73341, wei=104.04514}
-        String[][] driver = new String[driverList.size()/5][3];
-        for(int i=0;i<driverList.size();i=i+5){
-            Object obj = driverList.get(i);
-            Map map = JSONObject.parseObject(JSONObject.toJSONString(obj), Map.class);
-            driver[i/5][0] = "driver"+map.get("id");
-            driver[i/5][1] = map.get("jing").toString();//是纬度
-            driver[i/5][2] = map.get("wei").toString(); //是经度
-        }
+//        String[][] driver = new String[driverList.size()/5][3];
+//        for(int i=0;i<driverList.size();i=i+5){
+//            Object obj = driverList.get(i);
+//            Map map = JSONObject.parseObject(JSONObject.toJSONString(obj), Map.class);
+//            driver[i/5][0] = "driver"+map.get("id");
+//            driver[i/5][1] = map.get("jing").toString();//是纬度
+//            driver[i/5][2] = map.get("wei").toString(); //是经度
+//        }
         //System.out.println(driver);
 
-        DistributionService distributionService = new DistributionService(1,driverList.size()/5,passenger,driver);
-        int[] result = distributionService.distribute();
-        System.out.println("派单结果"+result[0]);
+//        DistributionService distributionService = new DistributionService(1,driverList.size()/5,passenger,driver);
+//        int[] result = distributionService.distribute();
+//        System.out.println("派单结果"+result[0]);
 
         //通知订单微服务更新状态
         Map<String,String> message=new HashMap<>();
         message.put("order_id",order_id);
-        message.put("driver_id",driver[result[0]][0]);
+//        message.put("driver_id",driver[result[0]][0]);
+        message.put("driver_id","sxy");       //测试直接返回247
         message.put("is_distributed","2");//1表示未派单，2表示已派单
         System.out.println(message);
 
         rabbitTemplate.convertAndSend("DispatchResponse","",JSON.toJSONString(message));
+    }
+    @Transactional
+    @RabbitListener(queues={"cancelDispatch"})
+    public void cancelOrderListen(Message messagePar){
+        String msg=new String(messagePar.getBody());
+        System.out.println("Start"+msg);
+
+        //获取订单信息'
+        JSONObject object=JSONObject.parseObject(msg);
+        System.out.println(object.toString());
+        String order_id=object.getString("order_id");
+        String driver_id=object.getString("driver_id");
+        //判断是否改变司机状态
+        if(driver_id.equals("")){
+            System.out.println(driver_id + "is null");
+            return;
+        }
+        Map<String,String> drivermessage=new HashMap<>();
+        drivermessage.put("driver_id",driver_id);
+        rabbitTemplate3.convertAndSend("ReleaseDriver","",JSON.toJSONString(drivermessage));
+        //通知订单微服务取消订单
+        Map<String,String> message=new HashMap<>();
+        message.put("order_id",order_id);
+        message.put("driver_id",driver_id);
+        message.put("is_distributed","1");//1表示未派单，2表示已派单
+        System.out.println(message);
+        rabbitTemplate2.convertAndSend("CancelFromDispatch","",JSON.toJSONString(message));
     }
 }
